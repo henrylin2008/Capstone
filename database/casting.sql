dfff23f5da2a53b9f86be1b914200cf8b11130c6
hlin <henrylin2008@yahoo.com>
2020-08-13 08:43:22 -0700

    postgreSQL database dump

---
+++ database/casting.sql
@@ -0,0 +1,193 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.3
+-- Dumped by pg_dump version 11.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: categories; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.actors (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    age integer,
+    gender character varying(8)
+);
+
+
+ALTER TABLE public.actors OWNER TO postgres;
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.actors_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.actors_id_seq OWNER TO postgres;
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.actors_id_seq OWNED BY public.actors.id;
+
+
+--
+-- Name: questions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movies (
+    id integer NOT NULL,
+    title character varying(100) NOT NULL,
+    release_date timestamp without time zone NOT NULL,
+    actor_id integer
+);
+
+
+ALTER TABLE public.movies OWNER TO postgres;
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.movies_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.movies_id_seq OWNER TO postgres;
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.movies_id_seq OWNED BY public.movies.id;
+
+
+--
+-- Name: categories id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.actors ALTER COLUMN id SET DEFAULT nextval('public.actors_id_seq'::regclass);
+
+
+--
+-- Name: questions id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies ALTER COLUMN id SET DEFAULT nextval('public.movies_id_seq'::regclass);
+
+
+--
+-- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.actors (id, name, age, gender) FROM stdin;
+1   Denzel Washington   65    male
+2   Tom Cruise  58  male
+3   Leonardo DiCaprio   45  male
+4   Samule Jackson  71  male
+5   Morgan Freeman  83  male
+6   Will Smith  51  male
+7   Matt Damon  49  male
+8   Brad Pitt   56  male
+9   Dwayne Johnson  48  male
+10  George Clooney  59  male
+11  Bruce Willis    65  male
+12  Mark Wahlberg   49  male
+13  Ryan Reynolds   43  male
+14  Liam Neeson 68  male
+15  Robert Downey Jr.   55  male
+16  Adam Sandler    53  male
+17  Vin Diesel  53  male
+18  Jason Statham   53  male
+19  Jackie Chan 66  male
+20  Donnie Yen  57  male
+\.
+
+
+--
+-- Data for Name: questions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movies (id, title, release_date) FROM stdin;
+1   The Dark Knight 2008-07-14
+2   Furious 7   2015-04-03
+3   Hobbs & Shaw    2019-07-13
+4   Slumdog Millionaire 2008-11-12
+5   Inception   2010-07-13
+6   Gladiator   2000-05-05
+7   Saving Private Ryan 1998-07-24
+8   Titanic 1997-12-19
+9   The Matrix  1999-03-31
+10  Rush Hour   1998-09-18
+11  Forrest Gump    1994-07-06
+12  Alien   1986-07-18
+13  Ocean's Eleven  2001-12-07
+14  Toy Story 4     2019-06-21
+15  300 2007-03-09
+\.
+
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.categories_id_seq', 6, true);
+
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.questions_id_seq', 10, true);
+
+
+--
+-- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.actors
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: questions questions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT questions_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
