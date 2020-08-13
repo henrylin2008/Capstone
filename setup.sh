832d7afac5be7da0b879d32dd2fa39c332856ebc
hlin <henrylin2008@yahoo.com>
2020-08-13 07:47:09 -0700

    init setup

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+export AUTH0_DOMAIN='.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='casting'
+export CLIENT_ID=''
+
+export FLASK_APP=app.py
+export FLASK_DEBUG=true
+export DATABASE_URL='postgres://'
 
