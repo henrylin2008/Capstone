2be4ff855f949d43d2c246a7404125358a891d12
hlin <henrylin2008@yahoo.com>
2020-08-15 11:22:24 -0700

    updated env configs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 export AUTH0_DOMAIN='dev-wmig32c8.us.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='casting_agency'
-
+export DATABASE_URL='postgres://localhost:5432/casting'
 export FLASK_APP=app.py
 export FLASK_DEBUG=true
-export DATABASE_URL='postgres://localhost:5432/casting'
-
+flask run --reload
