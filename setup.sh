3a3e17f81d52369a30760ea4fdd40c61b8d9eb6e
hlin <henrylin2008@yahoo.com>
2020-08-14 22:54:26 -0700

    update setup configs

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-export AUTH0_DOMAIN='.us.auth0.com'
+export AUTH0_DOMAIN='dev-wmig32c8.us.auth0.com'
 export ALGORITHMS=['RS256']
-export API_AUDIENCE='casting'
-export CLIENT_ID=''
+export API_AUDIENCE='casting_agency'
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=true
-export DATABASE_URL='postgres://'
+export DATABASE_URL='postgres://localhost:5432/casting'
 
